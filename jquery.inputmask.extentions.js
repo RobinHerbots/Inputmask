f599b4233f60c4c676b41d70962ea6201e11055e
Robin Herbots <robin@robin-Latitude-E6510.(none)>
2011-08-02 00:12:58 +0200

    new file:   jquery.inputmask.extentions.js 	modified:   README

---
+++ jquery.inputmask.extentions.js
@@ -0,0 +1,14 @@
+/*
+Input Mask plugin extentions
+http://github.com/RobinHerbots/jquery.inputmask
+Copyright (c) 2010 Robin Herbots
+Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
+
+Optional extentions on the jquery.inputmask base
+*/
+
+$.extend($.inputmask.defaults.aliases, {
+        'date': {
+            "mask": "d/m/y"
+        }
+});
