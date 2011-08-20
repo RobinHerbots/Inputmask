eaa88a81906f9289d37c9fde42228923e748c4cf
Robin Herbots <robin@robin-Latitude-E6510.(none)>
2011-08-20 23:17:10 +0200

    modified:   jquery.inputmask.js Prevent inputmask from loading/executing more then once

--- jquery.inputmask.js
+++ jquery.inputmask.js
@@ -3,12 +3,13 @@ Input Mask plugin for jquery
 http://github.com/RobinHerbots/jquery.inputmask
 Copyright (c) 2010 Robin Herbots
 Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-Version: 0.4.1
+Version: 0.4.2
  
 This plugin is based on the masked input plugin written by Josh Bush (digitalbush.com)
 */
 
 (function($) {
+if($.fn.inputmask == undefined){
     $.inputmask = {
         //options default
         defaults: {
@@ -719,5 +720,5 @@ This plugin is based on the masked input plugin written by Josh Bush (digitalbus
 
         }
     };
-
+}
 })(jQuery);
