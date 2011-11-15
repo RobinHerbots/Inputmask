5cd2c2196e650a05e44cb8fee69b8fd4c991cb81
Robin Herbots <robin@robin-Latitude-E6510.(none)>
2011-11-15 23:29:40 +0100

    set version to 0.4.5

--- jquery.inputmask.js
+++ jquery.inputmask.js
@@ -3,7 +3,7 @@ Input Mask plugin for jquery
 http://github.com/RobinHerbots/jquery.inputmask
 Copyright (c) 2010 Robin Herbots
 Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-Version: 0.4.4
+Version: 0.4.5
  
 This plugin is based on the masked input plugin written by Josh Bush (digitalbush.com)
 */
