81940ea898434d72913957a5ff84494f7b97decb
Robin Herbots <robin.herbots@gmail.com>
2012-09-14 16:35:55 +0300

    Update js/jquery.inputmask.js

--- js/jquery.inputmask.js
+++ js/jquery.inputmask.js
@@ -3,7 +3,7 @@
 * http://github.com/RobinHerbots/jquery.inputmask
 * Copyright (c) 2010 - 2012 Robin Herbots
 * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-* Version: 1.0.23
+* Version: 1.0.23a
 */
 
 (function ($) {
