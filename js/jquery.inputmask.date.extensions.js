c5424bf79cf707f395bfc0c832ceeff708e6cef9
Robin Herbots <robin.herbots@gmail.com>
2012-11-04 22:33:05 +0100

    Set version to 1.2

--- js/jquery.inputmask.date.extensions.js
+++ js/jquery.inputmask.date.extensions.js
@@ -3,7 +3,7 @@ Input Mask plugin extensions
 http://github.com/RobinHerbots/jquery.inputmask
 Copyright (c) 2010 - 2012 Robin Herbots
 Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-Version: 1.0.6
+Version: 1.2.0
 
 Optional extensions on the jquery.inputmask base
 */
