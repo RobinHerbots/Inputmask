303b09cf714564785dcd57350873c3b5ae8d4a30
Robin Herbots <roh@laptop114.Reynaers.org>
2013-01-24 15:25:11 +0100

    fix isComplete function

--- js/jquery.inputmask.js
+++ js/jquery.inputmask.js
@@ -3,7 +3,7 @@
 * http://github.com/RobinHerbots/jquery.inputmask
 * Copyright (c) 2010 - 2012 Robin Herbots
 * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-* Version: 1.2.8b
+* Version: 1.2.8c
 */
 
 (function ($) {
@@ -147,7 +147,7 @@
                         opts.greedy = this.data('inputmask')['greedy'];
                         opts.repeat = this.data('inputmask')['repeat'];
                         opts.definitions = this.data('inputmask')['definitions'];
-						return isComplete(this);
+						return isComplete(this[0]);
                     default:
                         //check if the fn is an alias
                         if (!resolveAlias(fn)) {
