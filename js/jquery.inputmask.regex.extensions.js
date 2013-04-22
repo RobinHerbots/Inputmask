e98c23c19df315f5fe3695d76f755ec78224b7ee
Robin Herbots <robin.herbots@gmail.com>
2013-04-22 23:53:39 +0200

    add regex extension skeleton

---
+++ js/jquery.inputmask.regex.extensions.js
@@ -0,0 +1,13 @@
+/*
+Input Mask plugin extensions
+http://github.com/RobinHerbots/jquery.inputmask
+Copyright (c) 2010 - 2013 Robin Herbots
+Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
+Version: 0.0.0
+
+Regex extensions on the jquery.inputmask base
+Allows for using regular expressions as a mask
+*/
+(function ($) {
+   
+})(jQuery);
