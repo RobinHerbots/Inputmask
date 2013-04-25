70d08f9f2ea602b0d67e50a8d5df9101c0f8960e
Robin Herbots <robin.herbots@gmail.com>
2013-04-25 10:30:38 +0200

    update regex extension

--- js/jquery.inputmask.regex.extensions.js
+++ js/jquery.inputmask.regex.extensions.js
@@ -9,5 +9,36 @@ Regex extensions on the jquery.inputmask base
 Allows for using regular expressions as a mask
 */
 (function ($) {
-   
+     $.extend($.inputmask.defaults.aliases, { // $(selector).inputmask("Regex", { regex: "a regex expression"}
+        'Regex': {
+			mask: "r",
+			greedy: false,
+			repeat: 10, //needs to be computed
+			regex: null,
+			regexSplit: null,
+            definitions: {
+                'r': {
+                    validator: function (chrs, buffer, pos, strict, opts) {
+						function analyseRegex() {
+							//implement me
+						}
+						if(opts.regexSplit == null){
+							opts.regexSplit = analyseRegex();
+						}
+						
+						var cbuffer =  buffer.splice(pos + 1, 0, chrs),
+							regexPart, isValid = false;
+						for(var i = 0; i < regexSplit.length; i++) {
+							regexPart += opts.regexSplit[i];
+							var exp = new RegExp(regexPart);
+							isValid = exp.test(cbuffer);
+							if(isValid) break;
+						}
+						
+						return isValid;
+				    }
+				}
+			}
+		}
+	});
 })(jQuery);
