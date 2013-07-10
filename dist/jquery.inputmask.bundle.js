cc93c1513e099459ea1efdcd979dfc481b286465
Robin Herbots <roh@LAPTOP214.Reynaers.org>
2013-07-10 13:46:13 +0200

    fix: focused input click other browsertab and back => caret positioning

--- dist/jquery.inputmask.bundle.js
+++ dist/jquery.inputmask.bundle.js
@@ -3,7 +3,7 @@
 * http://github.com/RobinHerbots/jquery.inputmask
 * Copyright (c) 2010 - 2013 Robin Herbots
 * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-* Version: 2.2.65
+* Version: 2.2.66
 */
 
 (function ($) {
@@ -1476,7 +1476,7 @@ Input Mask plugin extensions
 http://github.com/RobinHerbots/jquery.inputmask
 Copyright (c) 2010 - 2013 Robin Herbots
 Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-Version: 2.2.65
+Version: 2.2.66
 
 Optional extensions on the jquery.inputmask base
 */
@@ -1578,7 +1578,7 @@ Input Mask plugin extensions
 http://github.com/RobinHerbots/jquery.inputmask
 Copyright (c) 2010 - 2012 Robin Herbots
 Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-Version: 2.2.65
+Version: 2.2.66
 
 Optional extensions on the jquery.inputmask base
 */
@@ -2047,7 +2047,7 @@ Input Mask plugin extensions
 http://github.com/RobinHerbots/jquery.inputmask
 Copyright (c) 2010 - 2013 Robin Herbots
 Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-Version: 2.2.65
+Version: 2.2.66
 
 Optional extensions on the jquery.inputmask base
 */
@@ -2214,7 +2214,7 @@ Input Mask plugin extensions
 http://github.com/RobinHerbots/jquery.inputmask
 Copyright (c) 2010 - 2013 Robin Herbots
 Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-Version: 2.2.65
+Version: 2.2.66
 
 Regex extensions on the jquery.inputmask base
 Allows for using regular expressions as a mask
