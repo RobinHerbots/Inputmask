45106831f9d2313ef1b19b0e9bd0041095cc27e6
Robin Herbots <roh@LAPTOP214.Reynaers.org>
2013-09-04 15:08:36 +0200

    initial phone extension - not working yet

--- dist/jquery.inputmask.bundle.js
+++ dist/jquery.inputmask.bundle.js
@@ -3,7 +3,7 @@
 * http://github.com/RobinHerbots/jquery.inputmask
 * Copyright (c) 2010 - 2013 Robin Herbots
 * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-* Version: 2.3.33
+* Version: 2.3.34
 */
 
 (function ($) {
@@ -1157,8 +1157,8 @@
                                 npt._valueSet = function (value) { this.value = isRTL ? value.split('').reverse().join('') : value; };
                             }
                             if ($.valHooks.text == undefined || $.valHooks.text.inputmaskpatch != true) {
-                                var valueGet = $.valHooks.text ? $.valHooks.text.get : function () { return this.value; };
-                                var valueSet = $.valHooks.text ? $.valHooks.text.set : function (value) { return this.value = value; };
+                                var valueGet = $.valHooks.text && $.valHooks.text.get ? $.valHooks.text.get : function () { return this.value; };
+                                var valueSet = $.valHooks.text && $.valHooks.text.set ? $.valHooks.text.set : function (value) { return this.value = value; };
 
                                 jQuery.extend($.valHooks, {
                                     text: {
@@ -1590,7 +1590,7 @@ Input Mask plugin extensions
 http://github.com/RobinHerbots/jquery.inputmask
 Copyright (c) 2010 - 2013 Robin Herbots
 Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-Version: 2.3.33
+Version: 2.3.34
 
 Optional extensions on the jquery.inputmask base
 */
@@ -1692,7 +1692,7 @@ Input Mask plugin extensions
 http://github.com/RobinHerbots/jquery.inputmask
 Copyright (c) 2010 - 2012 Robin Herbots
 Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-Version: 2.3.33
+Version: 2.3.34
 
 Optional extensions on the jquery.inputmask base
 */
@@ -2169,7 +2169,7 @@ Input Mask plugin extensions
 http://github.com/RobinHerbots/jquery.inputmask
 Copyright (c) 2010 - 2013 Robin Herbots
 Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-Version: 2.3.33
+Version: 2.3.34
 
 Optional extensions on the jquery.inputmask base
 */
@@ -2338,7 +2338,7 @@ Input Mask plugin extensions
 http://github.com/RobinHerbots/jquery.inputmask
 Copyright (c) 2010 - 2013 Robin Herbots
 Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-Version: 2.3.33
+Version: 2.3.34
 
 Regex extensions on the jquery.inputmask base
 Allows for using regular expressions as a mask
@@ -2503,3 +2503,81 @@ Allows for using regular expressions as a mask
         }
     });
 })(jQuery);
+﻿/*
+Input Mask plugin extensions
+http://github.com/RobinHerbots/jquery.inputmask
+Copyright (c) 2010 - 2013 Robin Herbots
+Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
+Version: 2.3.34
+
+Phone extension based on inputmask-multi - DO NOT USE YET!!  in TEST
+*/
+$.extend($.inputmask.defaults.aliases, {
+    'phone': {
+        url: "phone-codes.json",
+        mask: function (opts) {
+            function masksSort(maskList, defs, match, key) {
+                maskList.sort(function (a, b) {
+                    var ia = 0, ib = 0;
+                    for (; (ia < a[key].length && ib < b[key].length) ;) {
+                        var cha = a[key].charAt(ia);
+                        var chb = b[key].charAt(ib);
+                        if (!match.test(cha)) {
+                            ia++;
+                            continue;
+                        }
+                        if (!match.test(chb)) {
+                            ib++;
+                            continue;
+                        }
+                        if ($.inArray(cha, defs) != -1 && $.inArray(chb, defs) == -1) {
+                            return 1;
+                        }
+                        if ($.inArray(cha, defs) == -1 && $.inArray(chb, defs) != -1) {
+                            return -1;
+                        }
+                        if ($.inArray(cha, defs) == -1 && $.inArray(chb, defs) == -1) {
+                            if (cha != chb) {
+                                return cha < chb ? -1 : 1;
+                            }
+                        }
+                        ia++;
+                        ib++;
+                    }
+                    for (; (ia < a[key].length || ib < b[key].length) ;) {
+                        if (ia < a[key].length && !match.test(a[key].charAt(ia))) {
+                            ia++;
+                            continue;
+                        }
+                        if (ib < b[key].length && !match.test(b[key].charAt(ib))) {
+                            ib++;
+                            continue;
+                        }
+                        if (ia < a[key].length) {
+                            return 1;
+                        }
+                        if (ib < b[key].length) {
+                            return -1;
+                        }
+                    }
+                    return 0;
+                });
+                return maskList;
+            }
+
+            var maskList = [];
+            $.ajax({
+                url: opts.url,
+                async: false,
+                dataType: 'json',
+                success: function (response) {
+                    maskList = response;
+                }
+            });
+
+            return masksSort(maskList, ['#'], /[0-9]|#/, "mask");
+        }
+    }
+});
+
+
---
+++ js/jquery.inputmask.phone.extensions.js
@@ -0,0 +1,78 @@
+﻿/*
+Input Mask plugin extensions
+http://github.com/RobinHerbots/jquery.inputmask
+Copyright (c) 2010 - 2013 Robin Herbots
+Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
+Version: 0.0.0
+
+Phone extension based on inputmask-multi - DO NOT USE YET!!  in TEST
+*/
+$.extend($.inputmask.defaults.aliases, {
+    'phone': {
+        url: "phone-codes.json",
+        mask: function (opts) {
+            function masksSort(maskList, defs, match, key) {
+                maskList.sort(function (a, b) {
+                    var ia = 0, ib = 0;
+                    for (; (ia < a[key].length && ib < b[key].length) ;) {
+                        var cha = a[key].charAt(ia);
+                        var chb = b[key].charAt(ib);
+                        if (!match.test(cha)) {
+                            ia++;
+                            continue;
+                        }
+                        if (!match.test(chb)) {
+                            ib++;
+                            continue;
+                        }
+                        if ($.inArray(cha, defs) != -1 && $.inArray(chb, defs) == -1) {
+                            return 1;
+                        }
+                        if ($.inArray(cha, defs) == -1 && $.inArray(chb, defs) != -1) {
+                            return -1;
+                        }
+                        if ($.inArray(cha, defs) == -1 && $.inArray(chb, defs) == -1) {
+                            if (cha != chb) {
+                                return cha < chb ? -1 : 1;
+                            }
+                        }
+                        ia++;
+                        ib++;
+                    }
+                    for (; (ia < a[key].length || ib < b[key].length) ;) {
+                        if (ia < a[key].length && !match.test(a[key].charAt(ia))) {
+                            ia++;
+                            continue;
+                        }
+                        if (ib < b[key].length && !match.test(b[key].charAt(ib))) {
+                            ib++;
+                            continue;
+                        }
+                        if (ia < a[key].length) {
+                            return 1;
+                        }
+                        if (ib < b[key].length) {
+                            return -1;
+                        }
+                    }
+                    return 0;
+                });
+                return maskList;
+            }
+
+            var maskList = [];
+            $.ajax({
+                url: opts.url,
+                async: false,
+                dataType: 'json',
+                success: function (response) {
+                    maskList = response;
+                }
+            });
+
+            return masksSort(maskList, ['#'], /[0-9]|#/, "mask");
+        }
+    }
+});
+
+
