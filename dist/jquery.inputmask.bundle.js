45a1553278ffc34b72a79d5183ca05623c580f66
Robin Herbots <roh@LAPTOP214.Reynaers.org>
2013-10-02 09:16:05 +0200

    rebuild

--- dist/jquery.inputmask.bundle.js
+++ dist/jquery.inputmask.bundle.js
@@ -3,7 +3,7 @@
 * http://github.com/RobinHerbots/jquery.inputmask
 * Copyright (c) 2010 - 2013 Robin Herbots
 * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-* Version: 2.3.50
+* Version: 2.3.51
 */
 
 (function ($) {
@@ -1679,7 +1679,7 @@ Input Mask plugin extensions
 http://github.com/RobinHerbots/jquery.inputmask
 Copyright (c) 2010 - 2013 Robin Herbots
 Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-Version: 2.3.50
+Version: 2.3.51
 
 Optional extensions on the jquery.inputmask base
 */
@@ -1781,7 +1781,7 @@ Input Mask plugin extensions
 http://github.com/RobinHerbots/jquery.inputmask
 Copyright (c) 2010 - 2012 Robin Herbots
 Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-Version: 2.3.50
+Version: 2.3.51
 
 Optional extensions on the jquery.inputmask base
 */
@@ -2265,7 +2265,7 @@ Input Mask plugin extensions
 http://github.com/RobinHerbots/jquery.inputmask
 Copyright (c) 2010 - 2013 Robin Herbots
 Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-Version: 2.3.50
+Version: 2.3.51
 
 Optional extensions on the jquery.inputmask base
 */
@@ -2434,7 +2434,7 @@ Input Mask plugin extensions
 http://github.com/RobinHerbots/jquery.inputmask
 Copyright (c) 2010 - 2013 Robin Herbots
 Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-Version: 2.3.50
+Version: 2.3.51
 
 Regex extensions on the jquery.inputmask base
 Allows for using regular expressions as a mask
@@ -2604,7 +2604,7 @@ Input Mask plugin extensions
 http://github.com/RobinHerbots/jquery.inputmask
 Copyright (c) 2010 - 2013 Robin Herbots
 Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-Version: 2.3.50
+Version: 2.3.51
 
 Phone extension based on https://github.com/andr-04/inputmask-multi
 When using this extension make sure you specify the correct url to get the masks
