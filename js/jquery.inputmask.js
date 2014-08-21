6d97289de1752f5415042f87eec5c7f3913bbc1e
Robin Herbots <robin.herbots@gmail.com>
2014-08-21 16:09:28 +0200

    fix commit when still focus

--- js/jquery.inputmask.js
+++ js/jquery.inputmask.js
@@ -1511,7 +1511,7 @@
                     $el.closest('form').bind("submit", function () { //trigger change on submit if any
                         if (valueOnFocus != getBuffer().join('')) {
                             $el.change();
-                        }
+                        } else $el[0]._valueSet(''); //clear masktemplete on submit and still has focus
                         if (opts.autoUnmask && opts.removeMaskOnSubmit) {
                             $el.inputmask("remove");
                         }
