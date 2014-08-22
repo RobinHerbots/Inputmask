594f6dfa3bb71c5fe45a412c5825abc025587c60
Robin Herbots <robin.herbots@gmail.com>
2014-08-22 23:25:08 +0200

    update qunit

--- qunit/tests_keepStatic.js
+++ qunit/tests_keepStatic.js
@@ -11,7 +11,6 @@ test("{ mask: [\"+55-99-9999-9999\", \"+55-99-99999-9999\", ], keepStatic: true 
 
     $("#testmask").remove();
 });
-$("#test3").inputmask("+55-99-9999|(99)-9999", { keepStatic: true });
 
 test("{ mask: \"+55-99-9999|(99)-9999\", keepStatic: true } - type 1212341234", function () {
     var $fixture = $("#qunit-fixture");
@@ -21,6 +20,17 @@ test("{ mask: \"+55-99-9999|(99)-9999\", keepStatic: true } - type 1212341234", 
     $("#testmask").Type("1212341234");
 
     equal(document.getElementById("testmask")._valueGet(), "+55-12-1234-1234", "Result " + document.getElementById("testmask")._valueGet());
+
+    $("#testmask").remove();
+});
+
+test("{ mask: ['(99) 9999-9999', '(99) 99999-9999'] } - val('1212341234')", function () {
+    var $fixture = $("#qunit-fixture");
+    $fixture.append('<input type="text" id="testmask" />');
+    $("#testmask").inputmask({ mask: ['(99) 9999-9999', '(99) 99999-9999'] });
+    $("#testmask").val("1212341234");
+
+    equal(document.getElementById("testmask")._valueGet(), "(12) 1234-1234", "Result " + document.getElementById("testmask")._valueGet());
 
     $("#testmask").remove();
 });
