13c27cf3c1bedbea05b452c393c7e09ef7c82352
Robin Herbots <robin.herbots@gmail.com>
2014-12-27 01:04:54 +0100

    add minificartion on dist/inputmask

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,12 +13,18 @@ module.exports = function (grunt) {
         var uglifyConfig = {};
         var srcFiles = grunt.file.expand(path + "/*.js");
         for (var srcNdx in srcFiles) {
-            var dstFile = srcFiles[srcNdx].replace("js/", "");
+            var dstFile = srcFiles[srcNdx].replace("js/", ""),
+                dstFileMin = dstFile.replace(".js", ".min.js");
             wrapAMDLoader(srcFiles[srcNdx], "build/" + dstFile, dstFile.indexOf("extension") == -1 ? ["jquery"] : ["jquery", "./jquery.inputmask"]);
             uglifyConfig[dstFile] = {
                 dest: 'dist/inputmask/' + dstFile,
                 src: "build/" + dstFile,
                 options: { banner: createBanner(dstFile), beautify: true, mangle: false, preserveComments: "some", ASCIIOnly: true }
+            };
+            uglifyConfig[dstFileMin] = {
+                dest: 'dist/inputmask/' + dstFileMin,
+                src: "build/" + dstFile,
+                options: { banner: createBanner(dstFileMin), preserveComments: "some", ASCIIOnly: true }
             };
         }
 
