03f13886299c8a88e42579da3b48b26796107deb
Robin Herbots <robin.herbots@gmail.com>
2015-01-28 12:12:34 +0100

    jquery.inputmask 3.1.58

--- dist/inputmask/jquery.inputmask.regex.extensions.js
+++ dist/inputmask/jquery.inputmask.regex.extensions.js
@@ -3,7 +3,7 @@
 * http://github.com/RobinHerbots/jquery.inputmask
 * Copyright (c) 2010 - 2015 Robin Herbots
 * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-* Version: 3.1.57
+* Version: 3.1.58
 */
 !function(factory) {
     "function" == typeof define && define.amd ? define([ "jquery", "./jquery.inputmask" ], factory) : factory(jQuery);
