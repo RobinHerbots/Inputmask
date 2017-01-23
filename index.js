d68a7df0297613cf7b69858e5b4d1f758f7ce3a8
Robin Herbots <robin.herbots@gmail.com>
2017-01-23 11:50:08 +0100

    update package.json

--- index.js
+++
@@ -1,8 +0,0 @@
-require("./dist/inputmask/inputmask.dependencyLib");
-module.exports = require("./dist/inputmask/inputmask");
-require("./dist/inputmask/inputmask.extensions");
-require("./dist/inputmask/inputmask.date.extensions");
-require("./dist/inputmask/inputmask.numeric.extensions");
-require("./dist/inputmask/inputmask.phone.extensions");
-require("./dist/inputmask/inputmask.regex.extensions");
-require("./dist/inputmask/jquery.inputmask");
