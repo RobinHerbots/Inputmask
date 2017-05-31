f06678d452fb031bd576da279192d5695835ab7f
Robin Herbots <robin.herbots@gmail.com>
2017-05-31 21:55:20 +0200

    remove phone codes from bundle

--- bundle.js
+++ bundle.js
@@ -5,11 +5,11 @@ import "./js/inputmask.extensions";
 import "./js/inputmask.numeric.extensions";
 import "./js/inputmask.phone.extensions";
 
-import "./js/phone-codes/phone-be";
-import "./js/phone-codes/phone-nl";
-import "./js/phone-codes/phone-ru";
-import "./js/phone-codes/phone-uk";
-import "./js/phone-codes/phone";
+// import "./js/phone-codes/phone-be";
+// import "./js/phone-codes/phone-nl";
+// import "./js/phone-codes/phone-ru";
+// import "./js/phone-codes/phone-uk";
+// import "./js/phone-codes/phone";
 
 import im from "./js/inputmask.js";
 import dependencyLib from "./js/dependencyLibs/inputmask.dependencyLib";
