dae5f5078761ff3f8c8edd628b3daf0f57be5047
Robin Herbots <robin.herbots@gmail.com>
2018-11-05 22:06:46 +0100

    rebuild

--- dist/inputmask/dependencyLibs/inputmask.dependencyLib.js
+++ dist/inputmask/dependencyLibs/inputmask.dependencyLib.js
@@ -3,7 +3,7 @@
 * https://github.com/RobinHerbots/Inputmask
 * Copyright (c) 2010 - 2018 Robin Herbots
 * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-* Version: 4.0.3-beta.5
+* Version: 4.0.3-beta.6
 */
 
 (function(factory) {
