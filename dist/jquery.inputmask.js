b295978e83c105da50f4e63ebe674acd2bd498b7
Robin Herbots <robin.herbots@gmail.com>
2019-11-15 14:35:26 +0100

    bump

--- dist/jquery.inputmask.js
+++ dist/jquery.inputmask.js
@@ -3,7 +3,7 @@
  * https://github.com/RobinHerbots/Inputmask
  * Copyright (c) 2010 - 2019 Robin Herbots
  * Licensed under the MIT license
- * Version: 5.0.0-beta.312
+ * Version: 5.0.0-beta.313
  */
 !function webpackUniversalModuleDefinition(root, factory) {
     if ("object" == typeof exports && "object" == typeof module) module.exports = factory(require("jquery")); else if ("function" == typeof define && define.amd) define([ "jquery" ], factory); else {
