c5eeb4faf1b769a82395cfffabd9025016d1b4bf
Robin Herbots <robin.herbots@gmail.com>
2019-12-18 09:24:18 +0100

    rebuild

--- dist/inputmask.js
+++ dist/inputmask.js
@@ -3,7 +3,7 @@
  * https://github.com/RobinHerbots/Inputmask
  * Copyright (c) 2010 - 2019 Robin Herbots
  * Licensed under the MIT license
- * Version: 5.0.1-beta.11
+ * Version: 5.0.1-beta.12
  */
 !function webpackUniversalModuleDefinition(root, factory) {
     if ("object" == typeof exports && "object" == typeof module) module.exports = factory(); else if ("function" == typeof define && define.amd) define([], factory); else {
