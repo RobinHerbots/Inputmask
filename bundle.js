3455c6db1d1fe1d909d4577c095492a905a64dac
Robin Herbots <robin.herbots@gmail.com>
2019-12-19 11:01:32 +0100

    add initial input-mask htmlelement

--- bundle.js
+++ bundle.js
@@ -1,4 +1,5 @@
 require("./lib/extensions/inputmask.extensions");
 require("./lib/extensions/inputmask.date.extensions");
 require("./lib/extensions/inputmask.numeric.extensions");
+require("./lib/inputmaskElement");
 module.exports = require("./lib/inputmask.js");
