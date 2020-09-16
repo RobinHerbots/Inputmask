a58ec14cf5309d74c01d1e74251dce719544e320
Robin Herbots <robin.herbots@gmail.com>
2020-09-16 22:51:40 +0200

    fix submit of readonly/disbled fields #2382 #2384

--- dist/inputmask.js
+++ dist/inputmask.js
@@ -3,7 +3,7 @@
  * https://github.com/RobinHerbots/Inputmask
  * Copyright (c) 2010 - 2020 Robin Herbots
  * Licensed under the MIT license
- * Version: 5.0.6-beta.13
+ * Version: 5.0.6-beta.14
  */
 !function webpackUniversalModuleDefinition(root, factory) {
     if ("object" == typeof exports && "object" == typeof module) module.exports = factory(); else if ("function" == typeof define && define.amd) define([], factory); else {
@@ -1468,7 +1468,7 @@
                         var imOpts = $.data(that, "_inputmask_opts");
                         $(that).off(), imOpts && new _inputmask.default(imOpts).mask(that);
                     } else {
-                        if ("setvalue" === e.type || "FORM" === this.nodeName || !(that.disabled || that.readOnly && !("keydown" === e.type && e.ctrlKey && 67 === e.keyCode || !1 === opts.tabThrough && e.keyCode === _keycode.default.TAB))) {
+                        if ([ "submit", "reset", "setvalue" ].includes(e.type) || "FORM" === this.nodeName || !(that.disabled || that.readOnly && !("keydown" === e.type && e.ctrlKey && 67 === e.keyCode || !1 === opts.tabThrough && e.keyCode === _keycode.default.TAB))) {
                             switch (e.type) {
                               case "input":
                                 if (!0 === inputmask.skipInputEvent || e.inputType && "insertCompositionText" === e.inputType) return inputmask.skipInputEvent = !1, 
