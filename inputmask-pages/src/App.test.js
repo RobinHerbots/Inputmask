a34d76a4f57137f8f35542df321f90baa3b7b82f
Robin Herbots <rherbots@biocartis.com>
2023-02-22 08:13:32 +0100

    Updates

--- inputmask-pages/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-
-import App from "./App";
-
-test("renders App", () => {
-  render(<App></App>);
-  const appContainer = screen.getByTestId("app-container");
-  expect(appContainer).toBeInTheDocument();
-});
--- inputmask-pages/src/components/Footer/Footer.js
+++
@@ -1,7 +0,0 @@
-import styles from "./Footer.module.scss";
-
-export const Footer = () => (
-  <div className={styles.Footer} data-testid="Footer">
-    Robin Herbots
-  </div>
-);
